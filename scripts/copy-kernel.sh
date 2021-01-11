e93c266c2d89e52ee4eb2996742288c691778087
Jacob McSwain <jmcswain@hashicorp.com>
2021-01-11 14:15:58 -0600

    Scripts: copy files with docker volumes

--- scripts/copy-kernel.sh
+++ scripts/copy-kernel.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -xe
+
 docker stop -t 1 charch-vmlinuz-copy || true
 docker rm charch-vmlinuz-copy || true
 
-docker run -t --name charch-vmlinuz-copy jamcswain/charch:ahead cat /usr/lib/modules/*/vmlinuz > $1
+docker run -v `pwd`:/kernout -t --name charch-vmlinuz-copy jamcswain/charch:ahead cp -v /usr/lib/modules/*/vmlinuz /kernout/$1
 
 docker stop -t 1 charch-vmlinuz-copy
 docker rm charch-vmlinuz-copy
--- scripts/generate-initramfs.sh
+++ scripts/generate-initramfs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -xe
+
 docker stop -t 1 charch-initramfs || true
 docker rm charch-initramfs || true
 
-docker run -t --name charch-initramfs jamcswain/charch:ahead mkinitcpio -p linux -g /dev/stdout > $1
+docker run -v `pwd`:/ramout -t --name charch-initramfs jamcswain/charch:ahead mkinitcpio -p linux -g /ramout/$1
 
 docker stop -t 1 charch-initramfs
 docker rm charch-initramfs
